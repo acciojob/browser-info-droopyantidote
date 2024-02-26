9199e9906960791e898456afcacbaa67b26440ff
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-26 09:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var browserInfoDiv = document.getElementById("browser-info");
+
+    if (browserInfoDiv) {
+        var browserName = navigator.appName;
+        var browserVersion = navigator.appVersion;
+
+        // Update the content of the browser-info div
+        browserInfoDiv.textContent = "You are using " + browserName + " version " + browserVersion;
+    } else {
+        console.error("The browser-info div is not found in the document.");
+    }
+});
